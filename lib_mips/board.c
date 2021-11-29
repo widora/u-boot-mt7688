37d2bb6a4ff8b12fb0aa101e3fefdc60f7840bab
yuanchenmang <771992497@qq.com>
2021-11-29 20:38:50 +0800

    incrementing version number - v1.1.0

--- lib_mips/board.c
+++ lib_mips/board.c
@@ -536,7 +536,7 @@ static int init_func_ram (void)
 static int display_banner(void)
 {
    
-	printf ("\n\nWidora by mango,V1.0.9\n\n");
+	printf ("\n\nWidora by mango,V1.1.0\n\n");
 	return (0);
 }
 
